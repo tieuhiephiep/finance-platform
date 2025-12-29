454523fb8b61b979b0b9df2668b125ab2b60cd6a
Hiep Quang <peipehpih@gmail.com>
2025-12-29 17:18:18 +0700

    update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,23 @@
-import { NextResponse } from 'next/server'
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { request } from 'http';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Định nghĩa các route cần bảo vệ (Dashboard và trang chủ nếu muốn)
 const isProtectedRoute = createRouteMatcher([
-  "/",
-])
+  '/dashboard(.*)',
+  // '/', // Bỏ comment dòng này nếu bạn muốn bắt buộc login ngay từ trang chủ
+]);
 
-export default clerkMiddleware(async (auth, request) => {
-  if (isProtectedRoute(request)) {
-    const { userId, redirectToSignIn } = await auth()
+export default clerkMiddleware(async (auth, req) => {
+  // Nếu route nằm trong danh sách cần bảo vệ, gọi hàm protect()
+  if (isProtectedRoute(req)) {
+    await auth.protect();
   }
-
-  return NextResponse.next();
 });
 
 export const config = {
-    matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+  matcher: [
+    // Bỏ qua các file tĩnh của Next.js và các file hình ảnh/font...
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    // Luôn chạy middleware cho các API route
     '/(api|trpc)(.*)',
   ],
-}
+};
